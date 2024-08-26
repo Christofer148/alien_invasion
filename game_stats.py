617ba005517ddf14cb30a0ec263c3b1130be9f61
Christofer <christofer.alcaraz148@gmail.com>
2024-08-26 14:25:28 -0600

    Adding the high score a displaying the ships left

--- game_stats.py
+++ game_stats.py
@@ -9,8 +9,11 @@ class GameStats():
         self.game_active = False
 
         self.reset_stats()
+        # High score shoul never be reset.
+        self.high_score = 0
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
+        self.level = 1
